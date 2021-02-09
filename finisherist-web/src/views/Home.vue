c1a8ada50e04c8d01baddbb6c158fd846a544c6a
Ha Nguyen <ndh103@gmail.com>
2021-02-09 21:12:55 +0700

    Add tailwind css

--- finisherist-web/src/views/Home.vue
+++ finisherist-web/src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <button
+      class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700"
+    >
+      Click me
+    </button>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Component({
   components: {
