505c93fc0af683e77e6eb0729caaf0b20f20ac96
Ha Nguyen <ndh103@gmail.com>
2021-03-07 15:57:32 +0700

    update authService using typescript

--- finisherist-web/src/auth/authService.ts
+++ finisherist-web/src/auth/authService.ts
@@ -1,13 +1,10 @@
 /* eslint-disable */
 import Oidc from "oidc-client"
-import { isJSDocReturnTag } from "typescript"
 
 var userManager = new Oidc.UserManager({
-  userStore: new Oidc.WebStorageStateStore(),
   authority: "https://localhost:5001",
   client_id: "web-client",
   redirect_uri: window.location.origin + "/auth/callback",
-  //response_type: "id_token token",
   response_type: "code",
   scope: "openid profile api",
   post_logout_redirect_uri: window.location.origin + "/",
@@ -21,89 +18,69 @@ var userManager = new Oidc.UserManager({
 Oidc.Log.logger = console
 Oidc.Log.level = Oidc.Log.INFO
 
-userManager.events.addUserLoaded(function(user) {
+userManager.events.addUserLoaded(function (user) {
   console.log("New User Loaded：", arguments)
   console.log("Acess_token: ", user.access_token)
 })
 
-userManager.events.addAccessTokenExpiring(function() {
+userManager.events.addAccessTokenExpiring(function () {
   console.log("AccessToken Expiring：", arguments)
 })
 
-userManager.events.addAccessTokenExpired(function() {
+userManager.events.addAccessTokenExpired(function () {
   console.log("AccessToken Expired：", arguments)
   alert("Session expired. Going out!")
   userManager
     .signoutRedirect()
-    .then(function(resp) {
+    .then(function (resp) {
       console.log("signed out", resp)
     })
-    .catch(function(err) {
+    .catch(function (err) {
       console.log(err)
     })
 })
 
-userManager.events.addSilentRenewError(function() {
+userManager.events.addSilentRenewError(function () {
   console.error("Silent Renew Error：", arguments)
 })
 
-userManager.events.addUserSignedOut(function() {
+userManager.events.addUserSignedOut(function () {
   alert("Going out!")
   console.log("UserSignedOut：", arguments)
   userManager
     .signoutRedirect()
-    .then(function(resp) {
+    .then(function (resp) {
       console.log("signed out", resp)
     })
-    .catch(function(err) {
+    .catch(function (err) {
       console.log(err)
     })
 })
 
 class AuthService {
-  // Renew the token manually
-  renewToken() {
-    let self = this
-    return new Promise((resolve, reject) => {
-      userManager
-        .signinSilent()
-        .then(function(user) {
-          if (user == null) {
-            self.signIn(null)
-          } else {
-            return resolve(user)
-          }
-        })
-        .catch(function(err) {
-          console.log(err)
-          return reject(err)
-        })
-    })
-  }
-
   // Get the user who is logged in
   getUser() {
     return userManager.getUser()
   }
 
-  // Check if there is any user logged in
+  // Check if there is any user logged in, if not, do the sign in
   signInIfHasNot() {
     var self = this;
     return userManager
       .getUser()
-      .then(function(user) {
+      .then(function (user) {
         if (user == null) {
           self.signinRedirect()
         }
       })
-      .catch(function(err) {
+      .catch(function (err) {
         console.log(err)
       })
   }
 
   // Redirect of the current window to the authorization endpoint.
   signinRedirect() {
-    userManager.signinRedirect().catch(function(err) {
+    userManager.signinRedirect().catch(function (err) {
       console.log(err)
     })
   }
@@ -120,12 +97,32 @@ class AuthService {
   signoutRedirect() {
     return userManager
       .signoutRedirect()
-      .then(function(resp) {
+      .then(function (resp) {
         console.log("signed out", resp)
       })
-      .catch(function(err) {
+      .catch(function (err) {
         console.log(err)
       })
+  }
+
+  // Renew the token manually
+  renewToken() {
+    let self = this
+    return new Promise((resolve, reject) => {
+      userManager
+        .signinSilent()
+        .then(function (user) {
+          if (user == null) {
+            self.signinRedirect()
+          } else {
+            return resolve(user)
+          }
+        })
+        .catch(function (err) {
+          console.log(err)
+          return reject(err)
+        })
+    })
   }
 }
 
