ceff6e1bb722c2daf80be4c562c09523115bf5b1
Ha Nguyen <ndh103@gmail.com>
2021-07-31 18:13:59 +0700

    Initial adding flyway into project, move sql folder outside

---
+++ PersonHubApi/pipeline/sql/V20210722142322__AddTodoItemOrderField.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "TodoItems" ADD "ItemOrder" text NULL;
---
+++ PersonHubApi/tools/flyway.bash
@@ -0,0 +1,1 @@
+flyway migrate -url=jdbc:postgresql://localhost:5432/person-hub-db -schemas="person-hub-test" -user=postgres -password=P@ssw0rd -locations=filesystem:../pipeline/sql
