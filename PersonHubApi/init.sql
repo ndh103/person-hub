29ab7550db6e09b6fea5e9e3b05a083a411d841e
Ha Nguyen <ndh103@gmail.com>
2021-08-02 08:53:15 +0700

    Clean up

--- PersonHubApi/init.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE "person-hub-db-test";
-GRANT ALL PRIVILEGES ON DATABASE "person-hub-db-test" TO postgres;
-
--- grant usage on schema public to public;
--- grant create on schema public to public;
-
