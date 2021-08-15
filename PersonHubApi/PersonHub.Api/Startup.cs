3e3e1d60a63d077da005fa832516d3adc6f68720
Ha Nguyen <ndh103@gmail.com>
2021-08-15 09:50:21 +0700

    Testing with CORS

--- PersonHubApi/PersonHub.Api/Startup.cs
+++ PersonHubApi/PersonHub.Api/Startup.cs
@@ -58,6 +58,8 @@ namespace PersonHub.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            app.UseCors("AllowAll");
+
             if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -75,8 +77,6 @@ namespace PersonHub.Api
             app.UseStaticFiles();
 
             app.UseRouting();
-
-            app.UseCors("AllowAll");
 
             app.UseAuthentication();
 
