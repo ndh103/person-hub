c8f6fec378ca227552b5f009272a8df3238d78c2
Ha Nguyen <ndh103@gmail.com>
2021-08-15 10:56:22 +0700

    TEst

--- PersonHubApi/PersonHub.Api/Startup.cs
+++ PersonHubApi/PersonHub.Api/Startup.cs
@@ -68,7 +68,7 @@ namespace PersonHub.Api
             app.UseRouting();
 
             app.UseCors(builder => {
-                builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().WithExposedHeaders("*");
+                builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
             });
 
             app.UseAuthentication();
