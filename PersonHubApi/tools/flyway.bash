bb95cee7e8b820bd6e70b4f59f880c1ed4345866
Nguyen Danh Ha <ndh103@gmail.com>
2021-10-03 19:32:14 +0700

    Add more tools for running flyway through container

--- PersonHubApi/tools/flyway.bash
+++ PersonHubApi/tools/flyway.bash
@@ -1,3 +1,8 @@
 # flyway migration
 # run this bash script FROM THE ROOT FOLDER 
-flyway migrate -url=jdbc:postgresql://localhost:5432/person-hub-db -schemas="person-hub" -user=postgres -password=P@ssw0rd -locations=filesystem:sql
+# flyway migrate -url=jdbc:postgresql://localhost:5432/person-hub-db -schemas="person-hub" -user=postgres -password=P@ssw0rd -locations=filesystem:sql
+
+docker-compose -f ./tools/docker-compose.flyway.yml up -d
+docker-compose -f ./tools/docker-compose.flyway.yml down
+
+
