c72030954921a92545b510d80b7a5d349e138a7c
Nguyen Danh Ha <ndh103@gmail.com>
2021-10-10 21:50:39 +0700

    Test

--- PersonHubApi/tests/PersonHub.IntegrationTest/Tests/TestHelper.cs
+++ PersonHubApi/tests/PersonHub.IntegrationTest/Tests/TestHelper.cs
@@ -13,5 +13,11 @@ namespace PersonHub.IntegrationTest.Tests
             int maxEnumValue = Convert.ToInt32(Enum.GetValues(typeof(TEnum)).Cast<TEnum>().Max());
             return maxEnumValue;
         }
+
+        public static bool EqualsUpToSeconds(this DateTime dt1, DateTime dt2)
+        {
+            return dt1.Year == dt2.Year && dt1.Month == dt2.Month && dt1.Day == dt2.Day &&
+                   dt1.Hour == dt2.Hour && dt1.Minute == dt2.Minute && dt1.Second == dt2.Second;
+        }
     }
 }
