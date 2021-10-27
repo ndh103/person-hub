88f9cf20dc1b82827caa869f01e8a5d40b1f7a26
Nguyen Danh Ha <ndh103@gmail.com>
2021-10-27 21:00:09 +0700

    Add edit inline functionality

--- person-hub-web/src/views/home/todos/TodoItemOverview.vue
+++ person-hub-web/src/views/home/todos/TodoItemOverview.vue
@@ -8,7 +8,7 @@
       <input v-model="isChecked" type="checkbox" class="rounded text-green-500 mr-3 mt-1" @click="markAsDone()" />
     </div>
 
-    <div>
+    <div v-if="!isEditMode">
       <a
         v-if="isValidHttpUrl(todoItemOverview.title)"
         class="font-medium cursor-pointer italic"
@@ -19,6 +19,24 @@
       >
       <span v-else class="font-medium" :class="{ 'line-through': isChecked }">{{ todoItemOverview.title }}</span>
     </div>
+
+    <div v-if="isEditMode" class="flex-grow">
+      <input ref="titleInput" v-model="newTitle" type="text" class="app-input w-full" placeholder="Input and press enter to add new ..." @keyup.enter="updateTitle()" />
+    </div>
+
+    <span v-if="!isEditMode" class="flex-grow"></span>
+
+    <div class="flex">
+      <div>
+        <PencilIcon v-if="!isEditMode" class="h-4 w-4 block invisible cursor-pointer action-icon" @click="toggleEditMode()" />
+      </div>
+      <div class="pr-2">
+        <CloseIcon v-if="isEditMode" class="h-4 w-4 block invisible cursor-pointer action-icon" @click="cancelUpdateTitle()" />
+      </div>
+      <div>
+        <SaveIcon v-if="isEditMode" class="h-4 w-4 block invisible cursor-pointer action-icon" @click="updateTitle()" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -28,10 +46,16 @@
   import TodoItemModel from './api-services/models/TodoItemModel'
   import todoItemApiService from './api-services/todo-item-api-service'
   import HandleIcon from '@/assets/handle-icon.svg?component'
+  import PencilIcon from '@/assets/pencil-icon.svg?component'
+  import SaveIcon from '@/assets/save-icon.svg?component'
+  import CloseIcon from '@/assets/close-icon.svg?component'
 
   export default defineComponent({
     components: {
       HandleIcon,
+      PencilIcon,
+      SaveIcon,
+      CloseIcon,
     },
     props: {
       todoItemOverview: {
@@ -43,11 +67,19 @@
     data() {
       return {
         isChecked: false,
+        isEditMode: false,
+        newTitle: '',
       }
     },
     methods: {
       gotoDetail() {
         this.$router.push(`/todos/${this.todoItemOverview.id}`)
+      },
+      toggleEditMode() {
+        this.newTitle = this.todoItemOverview.title
+        this.isEditMode = true
+
+        this.$nextTick(() => this.$refs.titleInput.focus())
       },
       async markAsDone() {
         var response = await todoItemApiService.markAsDone(this.todoItemOverview.id)
@@ -58,6 +90,22 @@
           // Error, reset the checkbox
           this.isChecked = false
         }
+      },
+      cancelUpdateTitle() {
+        this.isEditMode = false
+        this.newTitle = ''
+      },
+      async updateTitle() {
+        var updatedItem = { ...this.todoItemOverview }
+        updatedItem.title = this.newTitle
+
+        var response = await todoItemApiService.update(updatedItem, true)
+        if (response) {
+          // eslint-disable-next-line vue/no-mutating-props
+          this.todoItemOverview.title = this.newTitle
+        }
+        this.isEditMode = false
+        this.newTitle = ''
       },
       isValidHttpUrl(string) {
         let url
@@ -75,7 +123,8 @@
 </script>
 
 <style lang="postcss" scoped>
-  .todo-item-overview:hover .handle-icon {
+  .todo-item-overview:hover .handle-icon,
+  .todo-item-overview:hover .action-icon {
     @apply visible;
   }
 
